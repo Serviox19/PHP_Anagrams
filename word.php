a2e0cf69502e59a75270db42bab00a0ddf311ff2
Servio Mora <servio.mora19@gmail.com>
2016-08-29 13:37:45 -0400

    change

--- word.php
+++ word.php
@@ -1,10 +1,12 @@
 <?php
 
 if (isset($_GET['word'])) {
-  $dict = array('race', 'boat', 'file', 'print');
+  // $dict = array('race', 'boat', 'file', 'print');
   $word = $_GET['word'];
 
   $word = strtolower($word);//need lowercase
+
+  $letters = str_split($word);
 
   for($i=0;$i<strlen($word);$i++) {
     $letters[$i]=substr($word,$i,1);
