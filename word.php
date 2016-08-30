c55c16c89b63dbfd1e94d4e802f67460a6f5e128
Servio Mora <servio.mora19@gmail.com>
2016-08-30 00:10:48 -0400

    almost there

--- word.php
+++ word.php
@@ -3,24 +3,20 @@
   if (isset($_REQUEST['word']) && !empty($_REQUEST['word'])) {
 
     $word = $_REQUEST['word'];
-    $dictionary = file('dict.txt');
     $word = strtolower($word);//need lowercase
     $letters = str_split($word);
 
-    echo $word;
+    // echo $word . "\n";
 
-    // foreach ($dictionary as $thisword) {
-    //   test($thisword, $word) {
-    //     echo "Anagram for \$word: $thisword[$i]. \n";
-    //   }
-    // }
-    //
-    // function test($word1, $word2) {
-    //   if (isAnagram($word1, $word2)) {
-    //     echo "true";
-    //   }
-    // }
-    //
+    $filename = 'dict.txt';
+    $contents = file($filename);
+
+    foreach ($contents as $line) {
+      //check for words with same amount of letters & are actually in the file.
+      // echo $line . "\n";
+    }
+
+
     // function isAnagram($a, $b) {
     //   return(count_chars($a, 1) == count_chars($b, 1));
     // }
