cf871355600fc94bdc2cf7f418e61fb9984c9453
Servio Mora <servio.mora19@gmail.com>
2016-10-19 20:10:38 -0400

    removed unnecessary js ajax and updated solution

--- index.php
+++ index.php
@@ -10,10 +10,46 @@
   </head>
   <body>
 
-    <form class="form-inline">
-      <input type="text" class="form-control" placeholder="Input Text" id="word">
-      <input type="submit" class="btn btn-primary" id="submit" value="Chat">
+    <form action=# method=GET>
+    	<input type="text" name="text">
+    	<input type="submit">
     </form>
+
+    <?php
+      $start=microtime(true);
+      $filename="dict.txt";
+      $online_dict="http://web.stanford.edu/class/cs106l/assignments/dictionary.txt";
+
+      function reorder_word($word){
+      	$word=str_split(trim(strtolower($word)));
+      	sort($word);
+      	return join($word);
+      }
+
+      if(!isset($_GET['text']))die();
+      $input=reorder_word($_GET['text']);
+      $len=strlen($input);
+
+      if(file_exists($filename)){
+      	$answer=array_keys(unserialize(file_get_contents($filename)),$input);
+      }else{
+      	$arr=$answer=array();
+      	foreach(file($online_dict) as $word){
+      		$word=trim($word);
+      		$word_f=reorder_word($word);
+      		$arr[$word]=$word_f;
+      		if(strlen($word_f)!=$len)continue;
+      		if($input==$word_f){
+      			$answer[]=$word;
+      		}
+      	}
+      	file_put_contents($filename,serialize($arr));
+      }
+      foreach($answer as $string)
+      	echo "$string<br>";
+
+      echo microtime(true)-$start;
+    ?>
 
     <div id="response"></div>
 
